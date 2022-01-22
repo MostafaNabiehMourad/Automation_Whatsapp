35c927ed5ab4b544975987ab609fe5f59450ee9d
MostafaNabiehMourad <81525051+MostafaNabiehMourad@users.noreply.github.com>
2022-01-22 10:37:45 +0200

    Add files via upload

---
+++ automation_whatsapp.py
@@ -0,0 +1,12 @@
+import pywhatkit
+
+pywhatkit.sendwhatmsg_instantly("Phone Nubmer", "اخبارك اية",tab_close=True,close_time=10)
+pywhatkit.sendwhatmsg_instantly("Phone Nubmer", "عامل اية النهاردة واخبارك اية في الشغل",tab_close=True,close_time=10)
+
+pywhatkit.sendwhats_image("Phone Nubmer", "download.jpg",tab_close=True,close_time=10)
+
+pywhatkit.sendwhatmsg_to_group_instantly("Group Name", "Hey All!",tab_close=True,close_time=10)
+
+
+
+
